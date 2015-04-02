343f61cd10721908e3f1fbed27a8b910cc2f0c78
StanDimitroff <standimitroff@gmail.com>
2015-04-02 13:01:08 +0300

    fix

---
+++ 01. Web-Services-Overview-Homework/01. DistanceCalculatorService/DistanceCalculator.svc.cs
@@ -0,0 +1,12 @@
+﻿namespace DistanceCalculator
+{
+    using System;
+
+    public class DistanceCalculator : IDistanceCalculator
+    {
+        public double CalculateDistance(Point startPoint, Point endPoint)
+        {
+            return Math.Sqrt(Math.Pow(endPoint.X - startPoint.X, 2) + Math.Pow(startPoint.Y - endPoint.Y, 2));
+        }
+    }
+}
